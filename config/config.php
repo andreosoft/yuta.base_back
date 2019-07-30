5d61832236bee510d6c8c1940d8c5d024cf3c435
andreosoft <AndreyZagorets@gmail.com>
2019-07-30 15:28:13 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,11 +6,11 @@ return [
         'host' => 'localhost',
         'db' => 'crmav100_test',
         'user' => 'crmav100_base',
-        'password' => 'XHSN1O2FyZMHO6c0'], 
+        'password' => 'xxxxxxx'], 
     'mail' => [
         'host' => 'smtp.yandex.ru',
         'username' => 'robot@pxdesign.ru',
-        'password' => 'zpoef3i8dhaksjw23',
+        'password' => 'xxxxxxxx',
         'from' => ['robot@pxdesign.ru', 'no-replay'],
         'SMTPSecure' => 'ssl',
         'port' => 465
